f025a0d0def52c5f36c131e75caffbab917e0498
Torres Ver <adriana.torres.ver@accenture.com>
2019-06-21 02:18:00 -0500

    Probando funcionalidad del componente Login

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Login from './components/Login'
-import SignUp from './components/SignUp'
+// import SignUp from './components/SignUp'
 
 function App() {
   return (
@@ -18,8 +18,8 @@ function App() {
       <Route exact path="/equipos" component={Equipos} />
       <Route exact path="/calendario" component={Calendario} /> */}
 
-      {/* <Login /> */}
-      <SignUp />
+      <Login />
+      {/* <SignUp /> */}
 
       {/* </Router> */}
   
