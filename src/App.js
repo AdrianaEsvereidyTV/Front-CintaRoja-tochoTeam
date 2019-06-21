e1ced34fe79772d1a669a6fdc6b4380e8450f721
Angel <alorenzo@gluo.mx>
2019-06-20 20:38:22 -0500

    Firebase y router

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import Login from './components/Login'
 function App() {
   return (
     <div className="App">
-      {}
+      <Router>
+
       <NavBar />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/registro" component={Registro} />
+      <Route exact path="/equipos" component={Equipos} />
+      <Route exact path="/calendario" component={Calendario} />
+
       <Login />
+
+      </Router>
   
     </div>
   );
